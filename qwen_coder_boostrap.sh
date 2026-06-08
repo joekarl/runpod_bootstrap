5fbb8c8aba7d330ec3271ba391eb9fe81f76c188
Karl Kirch <karlkrch@gmail.com>
2026-06-08 05:14:06 +0000

    Add qwen script

---
+++ qwen_coder_boostrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# run.sh
+
+# Start script 
+# curl -sL https://raw.githubusercontent.com/joekarl/runpod_bootstrap/qwen_coder_boostrap.sh -o /workspace/qwen_coder_boostrap.sh && bash /workspace/qwen_coder_boostrap.sh
+# Container disk size: 18gb
+# Volume disk size : 55gb
+# Volume mount path : /workspace
+# expose http 11434
+
+# Install Ollama
+curl -fsSL https://ollama.com/install.sh | sh
+
+# Start Ollama
+ollama serve > /workspace/ollama.log 2>&1 &
+sleep 5
+ollama pull qwen3.6:35b
