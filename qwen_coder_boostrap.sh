0e21e422b549d9bebe28a96e4cfcb50b39ce8f36
Karl Kirch <karlkrch@gmail.com>
2026-06-08 05:30:37 +0000

    test

--- qwen_coder_boostrap.sh
+++ qwen_coder_boostrap.sh
@@ -9,9 +9,9 @@
 # expose http 11434
 
 # Install Ollama
-curl -fsSL https://ollama.com/install.sh | sh
+# curl -fsSL https://ollama.com/install.sh | sh
 
 # Start Ollama
-ollama serve > /workspace/ollama.log 2>&1 &
-sleep 5
-ollama pull qwen3.6:35b
+# ollama serve > /workspace/ollama.log 2>&1 &
+# sleep 5
+# ollama pull qwen3.6:35b
